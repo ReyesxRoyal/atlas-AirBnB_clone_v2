f43b4ee8d758a68e52094a383974e595f39e3a73
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-23 14:13:14 -0500

    updated user

--- models/user.py
+++ models/user.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
-# user.py
-
+#!/usr/bin/python3
+""" User Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Representation of User"""
+    """ A user """
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    
+    places = relationship("Place", cascade="all, delete-orphan", backref="user")
+
 
