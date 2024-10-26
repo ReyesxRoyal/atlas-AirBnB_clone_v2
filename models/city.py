9820c975f3220e580788e3f96c7f642e18abe782
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 17:18:31 -0500

    updated city

--- models/city.py
+++ models/city.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
-
 class City(BaseModel, Base):
     """Representation of city"""
     __tablename__ = 'cities'
@@ -12,8 +11,19 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    places = relationship("Place", cascade="all, delete-orphan", backref="city")
+    # Relationship with State
+    state = relationship("State", backref="cities")
 
     def __init__(self, *args, **kwargs):
         """Initializes the city instance"""
         super().__init__(*args, **kwargs)
+
+    def to_dict(self):
+        """Return a dictionary representation of the City instance"""
+        city_dict = super().to_dict()  # Call to_dict of BaseModel
+        city_dict.update({
+            'state_id': self.state_id,
+            'name': self.name,
+        })
+        return city_dict
+
