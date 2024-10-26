a634cb993a017e6f0acee23e74fe68e6d378cff0
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 17:35:00 -0500

    updated city for Q7

--- models/city.py
+++ models/city.py
@@ -14,16 +14,31 @@ class City(BaseModel, Base):
     # Relationship with State
     state = relationship("State", backref="cities")
 
+    # Relationship with Place
+    places = relationship("Place", cascade="all, delete-orphan", backref="city")
+
     def __init__(self, *args, **kwargs):
-        """Initializes the city instance"""
+        """Initializes city instance"""
         super().__init__(*args, **kwargs)
 
+    @property
+    def places(self):
+        """Returns the list of Place instances associated with the current City."""
+        from models import storage
+        place_list = []
+        place_dict = storage.all(Place)
+        for place in place_dict.values():
+            if place.city_id == self.id:
+                place_list.append(place)
+        return place_list
+
     def to_dict(self):
-        """Return a dictionary representation of the City instance"""
+        """Return a dictionary representation of the City instance."""
         city_dict = super().to_dict()  # Call to_dict of BaseModel
         city_dict.update({
-            'state_id': self.state_id,
             'name': self.name,
+            'state_id': self.state_id,
         })
         return city_dict
+
 
