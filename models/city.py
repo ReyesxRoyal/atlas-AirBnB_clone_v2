ef356f1069ede5529f5805744704a0663e01eae6
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 17:00:47 -0500

    city and state pushed

--- models/city.py
+++ models/city.py
@@ -2,20 +2,18 @@
 # models/city.py
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
 from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-
     """Representation of city"""
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    place = relationship("Place", cascade="all, delete-orphan"), backref="city")
+    places = relationship("Place", cascade="all, delete-orphan", backref="city")
 
     def __init__(self, *args, **kwargs):
-        super().__init__(args, **kwargs)
-        
+        """Initializes the city instance"""
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -29,3 +29,7 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+
+    def __init__(self, *args, **kwargs):
+        """Initializes state instance"""
+        super().__init__(*args, **kwargs)
