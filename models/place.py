198374d1c2ee9d2b4acc723ec651ec647960fbb7
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 17:28:03 -0500

    updated place

--- models/place.py
+++ models/place.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+# models/place.py
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """Representation of a place"""
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -18,4 +17,25 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the place instance"""
+        super().__init__(*args, **kwargs)
+
+    def to_dict(self):
+        """Return a dictionary representation of the Place instance"""
+        place_dict = super().to_dict()  # Call to_dict of BaseModel
+        place_dict.update({
+            'city_id': self.city_id,
+            'user_id': self.user_id,
+            'name': self.name,
+            'description': self.description,
+            'number_rooms': self.number_rooms,
+            'number_bathrooms': self.number_bathrooms,
+            'max_guest': self.max_guest,
+            'price_by_night': self.price_by_night,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+        })
+        return place_dict
+
