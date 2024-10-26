c842fe749072501f446a3834da9dcd1b6ad06472
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 17:22:15 -0500

    updated user

--- models/user.py
+++ models/user.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-""" User Module for HBNB project """
-from models.base_model import BaseModel, Base
+# models/user.py
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
+from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
-    """ A user """
+    """Representation of a user"""
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    
-    places = relationship("Place", cascade="all, delete-orphan", backref="user")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the user instance"""
+        super().__init__(*args, **kwargs)
+
+    def to_dict(self):
+        """Return a dictionary representation of the User instance"""
+        user_dict = super().to_dict()  # Call to_dict of BaseModel
+        user_dict.update({
+            'email': self.email,
+            'password': self.password,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+        })
+        return user_dict
+
 
 
