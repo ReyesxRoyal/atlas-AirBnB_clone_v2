39a522da2ef0e21614e8a1c3e9435c735c729d95
ReyesxRoyal <rasheed.r.white@gmail.com>
2024-10-29 12:49:46 -0500

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except BaseException:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -66,5 +66,5 @@ class FileStorage:
                 del self.__objects[key]
 
     def close(self):
-        """ calls reload method to deserialize json file to objects """
+        """call reload() method for deserializing the JSON file to objects"""
         self.reload()
