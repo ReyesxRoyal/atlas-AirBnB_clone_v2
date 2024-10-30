aea3ea66f4f6aebb4b8d57459a6fbffc79b82688
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-10-29 22:31:18 -0500

    Update amenity.py
    Amenity inherits from BaseModel and Base (respect the order)
    Add or replace in the class Amenity:
    class attribute __tablename__
    represents the table name, amenities
    class attribute name
    represents a column containing a string (128 characters)
    can’t be null
    class attribute place_amenities must represent a relationship Many-To-Many between the class Place and Amenity. Please see below more detail: place_amenity in the Place update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
+class Amenity(BaseModel, Base):
+    """Representation of an amenity"""
+    __tablename__ = 'amenities'
+    
+    name = Column(String(128), nullable=False)
 
-class Amenity(BaseModel):
-    name = ""
+    # Many-To-Many relationship between Place and Amenity
+    place_amenities = relationship("Place", secondary="place_amenity", backref="amenities")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the amenity instance"""
+        super().__init__(*args, **kwargs)
+
+    def to_dict(self):
+        """Return a dictionary representation of the Amenity instance"""
+        amenity_dict = super().to_dict()  # Call to_dict of BaseModel
+        amenity_dict.update({
+            'name': self.name,
+        })
+        return amenity_dict
